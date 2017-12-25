17a1d50c632717b51b274b16b72381ca4cf12cc1
Evgeniia Ershova <evgeniia_ershova@epam.com>
2017-12-25 17:46:05 +0300

    remove product factory

--- src/main/java/patterns/factory/StandardProductFactory.java
+++
@@ -1,9 +0,0 @@
-package patterns.factory;
-
-public class StandardProductFactory extends ProductFactory {
-
-    @Override
-    public Product createProduct() {
-        return new StandardProduct();
-    }
-}
--- src/main/java/patterns/factory/TrialProductFactory.java
+++
@@ -1,8 +0,0 @@
-package patterns.factory;
-
-public class TrialProductFactory extends ProductFactory {
-    @Override
-    public Product createProduct() {
-        return new TrialProduct();
-    }
-}
